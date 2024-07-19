c12295a6c3d7780bc7395e7846f86c62f724b011
root <root@t1v-n-48dfcea5-w-0.us-central1-a.c.felafax-proxy-firebase.internal>
2024-07-19 16:15:48 +0000

    Switched to FSDP tuning (merge jul19-fsdp-tuning-branch)
    backup -- implementing fsdp
    
    backup -- implementing fsdp
    
    backup -- train loop works -- just move into train loop fn and use xm spawn
    
    imagenet training loop, take code from this
    
    cleaned up imagenet training loop to bare essentials that I need to imp
    
    basic training loop ready
    
    basic training loop ready -- not working
    
    fixed bunch of issues found while running, fixed in both python and ipynb
    
    backup
    
    backup
    
    changed to tpu v5e
    
    changed to v5p
    
    docker installation failed, retrying with different config
    
    added fsdp test
    
    backup with making code same as fsdp test but tstill fails
    
    llama3 tpu works
    
    llama3 tpu works with actual HF dataset
    
    llama 2 7b works
    
    moved apply_fsdp on model into the util code itself
    
    update ntoebook file with tpu_works, added readme
    
    reverted back to apply_fsdp and fsdp_wrapper
    
    llama3 8b also works but OOMs after 1 step, resource exhausted
    
    added xmp spawn to llama3 tpu works
    
    deleted old llama3 file
    
    moved hugging face token to .env

---
+++ utils/fsdp.py
@@ -0,0 +1,33 @@
+import torch
+import torch_xla.core.xla_model as xm
+from torch_xla.distributed.fsdp import XlaFullyShardedDataParallel as FSDP
+from transformers import AutoModelForCausalLM, AutoConfig
+from peft import LoraConfig, TaskType, get_peft_model
+
+def fsdp_wrapper(x):
+    return FSDP(x, 
+                shard_param_on_dim_0=True, 
+                pin_layout_in_collective_ops=True, 
+                disable_reshard_on_root=False, 
+                reshard_after_forward=True)
+
+
+def apply_fsdp(module, class_names):
+    for k, v in getattr(module, "_modules").items():
+        if isinstance(v, (torch.nn.ModuleList, torch.nn.Sequential)):
+            for i, layer in enumerate(v):
+                clsname = layer.__class__.__name__
+                if clsname in class_names:
+                    v[i] = fsdp_wrapper(layer)
+                    # print("Applying fsdp wrapper: ", layer.__class__.__name__)
+                else:
+                    apply_fsdp(layer, class_names)
+                    
+        elif isinstance(v, torch.nn.Module):
+            clsname = v.__class__.__name__
+
+            if clsname in class_names:
+                setattr(module, k, fsdp_wrapper(v))
+                # print("Applying fsdp wrapper: ", module.__class__.__name__)
+            else:
+                apply_fsdp(v, class_names)
