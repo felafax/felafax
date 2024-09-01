36c7f0262b264d004c0adb9bda8cd7dd67f2fe1e
Felarof <nithin.sonti@gmail.com>
2024-09-01 03:30:01 +0000

    bunch of small import fixes and moved cross entropy to jax_utils

--- llama3_jax/llama_train.py
+++ llama3_jax/llama_train.py
@@ -57,29 +57,6 @@ model_path, model, model_configurator, tokenizer = automodel_lib.AutoJAXModelFor
     "llama-3.1-8B-JAX", HUGGINGFACE_TOKEN)
 
 
-def cross_entropy_loss_and_accuracy(logits, tokens, valid=None):
-    if valid is None:
-        valid = jnp.ones(tokens.shape[:2])
-    valid = valid.astype(jnp.float32)
-    valid_text_length = jnp.maximum(jnp.sum(valid, axis=-1), 1e-10)
-    logits = logits.astype(jnp.float32)  # for numerical stability
-    token_log_prob = jnp.squeeze(
-        jnp.take_along_axis(
-            jax.nn.log_softmax(logits, axis=-1),
-            jnp.expand_dims(tokens, -1),
-            axis=-1,
-        ),
-        -1,
-    )
-    token_log_prob = jnp.where(valid > 0.0, token_log_prob, jnp.array(0.0))
-    loss = -jnp.mean(jnp.sum(token_log_prob, axis=-1) / valid_text_length)
-    correct = jnp.where(valid > 0.0,
-                        jnp.argmax(logits, axis=-1) == tokens,
-                        jnp.array(False))
-    accuracy = jnp.mean(jnp.sum(correct, axis=-1) / valid_text_length)
-    return loss, accuracy
-
-
 def get_dataset(*, tokenizer, batch_size=1, seq_length=32, max_examples=None):
     # Define Alpaca prompt template
     alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
