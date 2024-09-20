6acbdc8efe16d86934a88c0a91af70807a6e1e04
Felarof <nithin.sonti@gmail.com>
2024-09-20 08:13:32 -0700

    3.1 405b

--- llama3_jax/trainer_engine/automodel_lib.py
+++ llama3_jax/trainer_engine/automodel_lib.py
@@ -29,7 +29,7 @@ MODEL_NAME_TO_DOWNLOAD_CONFIG = {
         "felafax_model_name": "felafax/llama-3.1-70B-Instruct-JAX",
         "chkpt_filename": "llama3.1_70b.flax",
     },
-    "llama-3.1-70B-Instruct-JAX": {
+    "llama-3.1-405B-Instruct-JAX": {
         "hf_model_name": "meta-llama/Meta-Llama-3.1-405B-Instruct",
         "felafax_model_name": "felafax/llama-3.1-405B-Instruct-JAX",
         "chkpt_filename": "llama3.1_405b.flax",
